489322dd7f112601a4872c8c3da649251e543504
ChaeDoll <dlacodbs0627@gmail.com>
2023-10-15 15:37:09 +0900

    add 문구

--- app/src/main/java/com/example/alcoholcalculator/MainActivity.kt
+++ app/src/main/java/com/example/alcoholcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.alcoholcalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        for (i in 0..9) Log.d("Test", "안녕하세요 $i")
+//        ArrayList<Material> materialList = new ArrayList<Material>()
         val changeButton: Button = findViewById(R.id.button)
         val textBox : TextView = findViewById(R.id.textView)
         val editText : EditText = findViewById(R.id.editText)
---
+++ app/src/main/java/com/example/alcoholcalculator/Material.kt
@@ -0,0 +1,5 @@
+package com.example.alcoholcalculator
+
+public class Material {
+
+}
